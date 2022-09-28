3b34579590f79f5469f5c5361128a0618127936c
ilker.eker <lkrkr@hotmail.com>
2022-09-29 00:15:49 +0300

    Add project files.

---
+++ CloudCustomers.UnitTests/Systems/Controllers/TestUsersController.cs
@@ -0,0 +1,120 @@
+using CloudCustomers.API.Controllers;
+using CloudCustomers.API.Models;
+using CloudCustomers.API.Services;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace CloudCustomers.UnitTests.Systems.Controllers
+{
+    public class TestUsersController
+    {
+        [Fact]
+        public async Task Get_OnSuccess_ReturnsStatusCode200()
+        {
+
+            //Arrange
+            var mockUsersService = new Mock<IUsersService>(); 
+            mockUsersService
+               .Setup(service => service.GetAllUsers())
+               .ReturnsAsync(new List<User>());
+
+            var sut = new UsersController(mockUsersService.Object);
+            //Act
+            var result = (OkObjectResult)await sut.Get();
+
+            //Assert
+            result.StatusCode.Should().Be(200);
+
+
+        }
+        [Fact]
+        public async Task Get_OnSuccess_InvokesUserServiceExactlyOnce()
+        {
+
+            //Arrange
+            var mockUsersService = new Mock<IUsersService>();
+            mockUsersService
+                .Setup(service=>service.GetAllUsers())
+                .ReturnsAsync(new List<User>());
+
+            var sut = new UsersController(mockUsersService.Object  );
+
+            //Act
+            var result = await sut.Get();
+
+            //Assert
+            mockUsersService.Verify(
+                service => service.GetAllUsers(),
+                Times.Once()
+            );
+
+
+        }
+        [Fact]
+        public async Task Get_OnSuccess_ReturnsListOfUsers()
+        {
+
+            //Arrange
+            var mockUsersService = new Mock<IUsersService>();
+            mockUsersService
+                .Setup(service => service.GetAllUsers())
+                .ReturnsAsync(new List<User>()
+                {
+
+                    new()
+                    {
+                        Id=1,
+                        Name="Ýlker",
+                        Address= new Address()
+                        {
+                            Street="123 Main St",
+                            City = "Madison",
+                            ZipCode="53704"
+                        },
+                        Email = "lkrkr@hotmail.com"
+
+                    }
+                }
+                
+                
+                
+                );
+
+            var sut = new UsersController(mockUsersService.Object);
+
+            //Act
+            var result = await sut.Get();
+
+            //Assert
+            result.Should().BeOfType<OkObjectResult>();
+
+            var objectResult = (OkObjectResult)result;
+
+            objectResult.Value.Should().BeOfType<List<User>>();
+
+        }
+        [Fact]
+        public async Task Get_OnNoUsersFound_Returns404()
+        {
+
+            //Arrange
+            var mockUsersService = new Mock<IUsersService>();
+            mockUsersService
+                .Setup(service => service.GetAllUsers())
+                .ReturnsAsync(new List<User>());
+
+            var sut = new UsersController(mockUsersService.Object);
+
+            //Act
+            var result = await sut.Get();
+
+            //Assert
+            result.Should().BeOfType<NotFoundResult>();
+
+          
+
+        }
+
+    }
+}
